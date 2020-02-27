331cfd9b1090ea079e43f0ffbf5325c3ae164ae6
batorinka <batorinka@list.ru>
2020-02-27 22:08:12 +0300

    Add AdminController

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeardShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeardShop.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        private AppDbContext db;
+        public AdminController(AppDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
